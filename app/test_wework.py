12cecda7a0573b4d00485686fc1376b9f8aeb675
32712784 <32712784@qq.com>
2020-10-31 21:01:20 +0800

    app测试练习

--- app/test_wework.py
+++ app/test_wework.py
@@ -33,6 +33,29 @@ class TestWework:
     def teardown(self):
         self.driver.quit()
 
+    def test_case1(self):
+        # 点击工作台
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='工作台']").click()
+        # 自动滚动屏幕点击进入打卡
+        self.driver.find_element(MobileBy.ANDROID_UIAUTOMATOR,
+                                 'new UiScrollable(new UiSelector()\
+                                 .scrollable(true).instance(0))\
+                                 .scrollIntoView(new UiSelector()\
+                                 .text("打卡").instance(0));').click()
+        # self.driver.find_element(MobileBy.XPATH, "//*[@text='打卡']").click()
+        # 设置idle等待时间为0(局部)
+        # self.driver.update_settings({"waitForIdleTimeout":0})
+        # 点击外出打卡
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='外出打卡']").click()
+        # 点击打卡
+        self.driver.find_element(MobileBy.XPATH, "//*[contains(@text,'次外出')]").click()
+        # 断言方式1
+        # sleep(2)
+        # assert "外出打卡成功" in self.driver.page_source
+        # 通过显示等待断言2
+        WebDriverWait(self.driver,10).until(lambda x : "外出打卡成功" in x.page_source)
+        self.driver.back()
+
     def test_demo(self):
         # el1 = self.driver.find_element_by_xpath(
         #     "/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.view.ViewGroup/android.widget.RelativeLayout[3]/android.widget.TextView")
@@ -47,25 +70,3 @@ class TestWework:
         self.driver.find_element_by_accessibility_id("xxxx")
         pass
 
-    def test_case1(self):
-        # 点击工作台
-        self.driver.find_element(MobileBy.XPATH, "//*[@text='工作台']").click()
-        # 点击进入打卡
-        self.driver.find_element(MobileBy.ANDROID_UIAUTOMATOR,
-                                 'new UiScrollable(new UiSelector()\
-                                 .scrollable(true).instance(0))\
-                                 .scrollIntoView(new UiSelector()\
-                                 .text("打卡").instance(0));').click()
-        # self.driver.find_element(MobileBy.XPATH, "//*[@text='打卡']").click()
-        # 设置idle等待时间为0(局部)
-        # self.driver.update_settings({"waitForIdleTimeout":0})
-        # 点击外出打卡
-        self.driver.find_element(MobileBy.XPATH, "//*[@text='外出打卡']").click()
-        # 点击打卡
-        self.driver.find_element(MobileBy.XPATH, "//*[contains(@text,'次外出')]").click()
-        # 断言方式1
-        sleep(2)
-        assert "外出打卡成功" in self.driver.page_source
-        # 通过显示等待断言2
-        WebDriverWait(self.driver,10).until(lambda x : "外出打卡成功" in x.page_source)
-        self.driver.back()
