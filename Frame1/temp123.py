e98e23ca037e44b30c7cabafb4fe1edf6754b16f
32712784 <32712784@qq.com>
2020-11-19 00:45:35 +0800

    自定义框架1

--- Frame1/temp123.py
+++ Frame1/temp123.py
@@ -1,16 +1,58 @@
 # coding=utf-8
-import builtins
 import importlib
-
 import yaml
 
 from rhythmSDET.Frame1 import hello
+# path,module, func,step
+def test_parse_yaml():
+    with open("./temp123.yaml") as f:
+        datas = yaml.load(f, Loader=yaml.FullLoader)
+        # datas = {'test_xxx': [{'hello.a': []}, {'print': ['abcde']}, {'re.search': ['.*', 'xxxx']}, {'save': ['temp']}, {'print': ['$(temp)']}]}
+        # print(datas)
+        for casename in datas.keys():
+            if casename.startswith("test_"):
+                print(f"{casename}是标准用例！！")
+                # datas[casename] =[{'rhythmSDET.Frame1.hello.test_a1': [1, 2]}, {'print': ['abcde']}, {'re.search': ['.*', 'xxxx']}, {'save': ['temp']}, {'print': ['$(temp)']}]
+                # print(datas[casename])
+                for step in datas[casename]:
+                    for mathod,params in step.items():
+                        # 如果包含. 说明是导入模块
+                        if "." in mathod:
+                            mathod_handle(mathod,params)
+                        elif mathod == "print":
+                            # 全局变量设置
+                            if "$" in params:
+                                pass
+                            else:
+                                print(*params)
+                            # 保存全局变量
+                        elif mathod == "save":
+                            pass
+                        else:
+                            print(f"{mathod}是未定义的方法！！")
+            else:
+                print(f"{casename}不是标准用例！！")
 
-def parse_yaml(path, module, func,step):
-    with open(path) as f:
-        content = yaml.load(f, Loader=yaml.FullLoader)
-        fun = content[func]
-        test_parse(module,fun, step)
+# 用于动态导入模块和方法
+def mathod_handle(mathod,params):
+    # list1 = ['rhythmSDET', 'Frame1', 'hello', 'a']
+    list1 = mathod.split(".")
+    # list2 = rhythmSDET.Frame1.hello
+    list2 = ".".join(list1[0:-1])
+    # list3 = test_a1
+    list3 = list1[-1]
+    main = importlib.import_module(list2)
+    # 如果入参不为0，params是list传入动态调用函数作为参数
+    if len(params) != 0:
+        if hasattr(main, list3):
+            # 得到：getattr(main, test_b1)(1, 2)
+            getattr(main, list3)(*params)
+            # print(getattr(main, list3)(*params))
+            print(f"{list2}.{list3}({params})")
+    else:
+        # 动态导入，执行无参数方法
+        if hasattr(main, list3):
+            getattr(main, list3)()
 
 def test_parse(module,fun,step):
     # fun = [{'print': [1234]}, {'str': 'a+b+c+d'}]
