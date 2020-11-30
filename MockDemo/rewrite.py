872e5ceb903647f63317114d74a0113087beb26c
32712784 <32712784@qq.com>
2020-12-01 01:58:00 +0800

    通过python 进行mock测试

--- MockDemo/rewrite.py
+++ MockDemo/rewrite.py
@@ -6,7 +6,7 @@ def response(flow: http.HTTPFlow):
     # 修改判断条件
     if "quote.json" in  flow.request.pretty_url and "x=" in flow.request.pretty_url:
         resp_json = json.loads(flow.response.content)
-        # print(resp_json)
+        print(resp_json)
         # 修改json中的数据
         resp_json["data"]["items"][0]["quote"]["name"] = "茅台霍格沃兹"
         resp_json["data"]["items"][1]["quote"]["name"] = resp_json["data"]["items"][1]["quote"]["name"]*2
